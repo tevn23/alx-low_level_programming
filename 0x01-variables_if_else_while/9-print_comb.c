8cc47a19cc8e96597a4cc83f17072690f9242626
Thakgatso <mabulatt23@gmail.com>
2022-09-10 05:29:24 +0000

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single digit numbers
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
