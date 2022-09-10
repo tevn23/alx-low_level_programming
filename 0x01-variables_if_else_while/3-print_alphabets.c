3e65befd28e91335a909086c808f8333d8803071
Thakgatso <mabulatt23@gmail.com>
2022-09-10 04:19:10 +0000

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - prints in lowercase, uppercase and followed by a new line
+ * 
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
