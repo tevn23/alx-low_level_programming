bc278258ab17a74127f1a218014615fe17c0c665
Thakgatso <mabulatt23@gmail.com>
2022-09-16 18:17:21 +0000

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests function if it works with positive or negative
+ *
+ * Return 0 Always
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Task 0 - Test if function works with case 0
