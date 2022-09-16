90b19fa9e63cda91b4933d89d140187a61732595
Thakgatso <mabulatt23@gmail.com>
2022-09-16 21:10:36 +0000

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,12 +11,12 @@ int main(void)
 	int i;
 
 	printf("Infinte loop incoming :(\n");
-	
+
 	i = 0;
 	/**
 	 * while (i < 10)
 	 * {
-	 * 	putchar(i);
+	 *	putchar(i);
 	 * }
 	 */
 	printf("Infinite loop avoided! \\o/\n");
