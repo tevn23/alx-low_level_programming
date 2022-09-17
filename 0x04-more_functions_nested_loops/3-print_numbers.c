8c6a2d55b989d4dda388b3519ca803a53b9a56bd
Thakgatso <mabulatt23@gmail.com>
2022-09-17 00:28:23 +0000

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for a digit
+ *
+ * Return: 0 Always
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 54; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
