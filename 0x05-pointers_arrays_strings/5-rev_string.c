129401605483ae01e3200e933d347a4a20476203
Thakgatso <mabulatt23@gmail.com>
2022-09-19 22:53:06 +0000

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,3 +18,19 @@ void rev_string(char *s)
 		*(s + len - i - 1) = tmp;
 	}
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
