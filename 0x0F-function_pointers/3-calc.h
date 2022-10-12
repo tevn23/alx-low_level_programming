3c76bcbd44c222b392f98ca480c59adc1aa344d8
Thakgatso <mabulatt23@gmail.com>
2022-10-12 22:28:24 +0000

    Task 3

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,23 @@
+#ifndef CALC_H
+#define CALC_H
+
+/**
+ * struct op - struct op
+ * @op: operator to use
+ * @f: function associated with operator
+ */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, b);
+} op_t;
+
+int main(int argc, char *argv[]);
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+
+#endif /* CALC_H */
