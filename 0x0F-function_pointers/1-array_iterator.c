63f33a2f5fadc563c277511042de22814bd404b9
Thakgatso <mabulatt23@gmail.com>
2022-10-12 22:00:33 +0000

    Task 1 Hot fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * array_iterator - executes function given as a prameter on each element of
+ * array_iterator - executes function given as a parameter on each element of
  * of an array
  * @array: array or list of elements
  * @size: the size of the array
+ * @action: action of array
  *
  * Return: void
  */
