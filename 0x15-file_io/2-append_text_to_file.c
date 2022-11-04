07b37b7a527a70afb7774d036f7ef77cc5e5ba9b
Thakgatso <mabulatt23@gmail.com>
2022-11-04 23:55:33 +0000

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,17 +23,17 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content)
-	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
+		text_content = "";
 
-		rwr = write(fd, text_content, nletters);
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-		if (rwr == -1)
-			return (-1);
-	}
+	rwr = write(fd, text_content, nletters);
 
-	close(fd)
+	if (rwr == -1)
+		return (-1);
 
-		return (1);
+	close(fd);
+
+	return (1);
 }
