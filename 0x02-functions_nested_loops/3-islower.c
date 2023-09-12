b61601a529becc8f3d2a8fb75bf557d38fa24690
tevn23 <mabulatt23@gmail.com>
2023-09-13 01:23:25 +0200

    Task 3 betty fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,14 @@
 
 /**
  * _islower - checks for lowercase character
+ * @c: character to be checked
  *
  * Return: 1 if c is lowercase and 0 if otherwise
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c<= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
