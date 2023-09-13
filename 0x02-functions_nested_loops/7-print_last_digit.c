312de3be60eb742ada98510f02f1bdc787ab76e3
tevn23 <mabulatt23@gmail.com>
2023-09-13 03:18:54 +0200

    Task 7 fix attempt

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,11 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
+	last_digit = n % 10;
 	if (n < 0)
 	{
 		last_digit = -n % 10;
 	}
-	else
-		last_digit = n % 10;
 
 	_putchar('0' + last_digit);
 	return (last_digit);
