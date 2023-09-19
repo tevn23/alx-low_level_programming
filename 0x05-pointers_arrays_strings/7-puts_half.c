f2c3bd477f99b445a670d7c0af939c0f03e87f36
tevn23 <mabulatt23@gmail.com>
2023-09-19 05:05:38 +0200

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: String to be printed
+ *
+ * Return: void
+ */
+int _strlen(char *s);
+
+void puts_half(char *str)
+{
+	int i;
+	int len = _strlen(str);
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	}
+	else
+	{
+		i = (len - 1) / 2;
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - returns length of a string
+ * @s: string to be inputted
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
