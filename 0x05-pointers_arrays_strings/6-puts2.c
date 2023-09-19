af6ccc9a6f03ec6120e75a358bfcd80afc985638
tevn23 <mabulatt23@gmail.com>
2023-09-19 04:25:57 +0200

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string starting with the 1st one
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+        int i;
+
+        for (i = 0; str[i] != '\0'; i = i + 2)
+                _putchar(str[i]);
+        _putchar('\n');
+}
