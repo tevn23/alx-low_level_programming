700feffc77aa7155736d8ff2f213ebbaf1368579
tevn23 <mabulatt23@gmail.com>
2023-09-22 02:41:02 +0200

    Task 3 fix 2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 < *s2)
 			return (-15);
 		else if (*s1 > *s2)
 			return (15);
+		s1++;
+		s2++;
 	}
-
-	/* end conditions */
 
 	if (*s1 == '\0' && *s2 == '\0')
 		return (0);
