c6c130f05453df6188f7a37bfa45a3d46227aaaf
tevn23 <mabulatt23@gmail.com>
2023-10-11 16:26:52 +0200

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer taking string as parameter
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
