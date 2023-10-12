c2163ae7701b43c4936c33bde71d4eba38a7d8f8
tevn23 <mabulatt23@gmail.com>
2023-10-12 04:40:01 +0200

    T-Task 1 fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != NULL)
+	if (array != NULL && action != NULL && size > 0)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
