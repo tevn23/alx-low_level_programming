6f193c454ceba805944462b63f057cd116a9f287
tevn23 <mabulatt23@gmail.com>
2023-10-12 04:53:52 +0200

    Task 2 betty fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,11 +15,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if(array != NULL && cmp != NULL && size > 0)
+	if (array != NULL && cmp != NULL && size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			if(cmp(array[i]) != 0)
+			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
