a2ace2c456f2fcc163d9c357efbceb6f85187223
tevn23 <mabulatt23@gmail.com>
2023-10-20 21:35:19 +0200

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len: returns the number of elements in a linked list_t list
+ * @h: singly linked list passed
+ *
+ * Return:no of elements printed
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		elements++;
+	}
+	return (elements);
+}
+
