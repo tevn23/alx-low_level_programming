6e6d1d05508ad5dadf7a0ac9744993968b48a964
tevn23 <mabulatt23@gmail.com>
2023-10-21 00:38:18 +0200

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,12 +2,37 @@
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
- * @head:
- * @str:
+ * @head: pointer to the head of the linked list
+ * @str: string to be duplicated and stored in the added node
  *
  * Return: address of a new element, or NULL if it failed
  */
 
-*add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
+	list_t *node;
+	char *dup_str;
+	
+	if (str == NULL)
+		return NULL;
+	
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+		return NULL;
+
+	node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+	{
+		free(node);
+		return NULL;
+	}
+
+	node->str = dup_str;
+	node->len = _strlen(dup_str);
+	node->next = *head;
+	*head = node;
+
+	return (node);
+}
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -24,5 +26,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(char *s);
 
 #endif /* LISTS_H */
