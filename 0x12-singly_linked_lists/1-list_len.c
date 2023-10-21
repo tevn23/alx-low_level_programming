cdec4ca59aa66291ce08b1bb83356cb7c4726fef
tevn23 <mabulatt23@gmail.com>
2023-10-21 09:43:06 +0200

    Task 1 betty fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len: returns the number of elements in a linked list_t list
+ * list_len - returns the number of elements in a linked list_t list
  * @h: singly linked list passed
  *
  * Return:no of elements printed
