69ef40f759cece5f02b6ead28c83a653a6a3bef6
tevn23 <mabulatt23@gmail.com>
2023-10-21 13:48:48 +0200

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: list to be freed
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
