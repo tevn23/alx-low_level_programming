7cf2863e4bea027217dd3a4c223dd8ba5c87927c
tevn23 <mabulatt23@gmail.com>
2023-10-25 00:32:51 +0200

    Task 5 fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,15 +12,15 @@ void free_listint2(listint_t **head)
 	listint_t *current, *temp;
 
 	if (head != NULL)
-	{
-		current = *head;
+		return;
 
-		while (current != NULL)
-		{
-			temp = current;
-			current = current->next;
-			free(temp);
-		}
+	current = *head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
 	}
 	*head = NULL;
 }
