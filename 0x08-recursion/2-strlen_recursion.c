7a511bdaf142592be3d535fc6fbd5f1f4f8b6991
tevn23 <mabulatt23@gmail.com>
2023-10-24 10:02:46 +0200

    Task 2 fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,13 +4,13 @@
  * _strlen_recursion - returns the length of a string
  * @s: string to be calculated
  *
- * Return: void
+ * Return: length of the string
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
-	else
-		return 1 + _strlen_recursion(s + 1);
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
 }
