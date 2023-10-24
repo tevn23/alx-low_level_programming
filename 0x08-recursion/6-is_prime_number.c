0d01be4e175ffb784b28cb4435fe47c25757f852
tevn23 <mabulatt23@gmail.com>
2023-10-24 14:20:20 +0200

    Task 6 no loops

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int is_prime_recursion(int n, int i);
 /**
  * is_prime_number - returns 1 if the input integer is a prime number
  * or 0 if not
@@ -7,14 +8,22 @@
  *
  * Return: 1 if prime number, 0 if otherwise
  */
-int is_prime_recursion(int n, int i);
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
 
-	return is_prime_recursion(n, n - 1);
+	return (is_prime_recursion(n, n - 1));
 }
+
+/**
+ * is_prime_recursion - checks is a number is prime or not
+ * through trial and error.
+ * @n: input integer to check
+ * @i: iterator to use as a divisor to n
+ *
+ * Return: 1 if prime and 0 if not
+ */
 
 int is_prime_recursion(int n, int i)
 {
