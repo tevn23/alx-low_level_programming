895e633949d374ebc1d5a8d7c538c294ac54d656
tevn23 <mabulatt23@gmail.com>
2023-10-24 14:15:41 +0200

    Task 6 no loops

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,18 +7,20 @@
  *
  * Return: 1 if prime number, 0 if otherwise
  */
-
+int is_prime_recursion(int n, int i);
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 		return (0);
 
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-	return (1);
+	return is_prime_recursion(n, n - 1);
+}
+
+int is_prime_recursion(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	return (is_prime_recursion(n, i - 1));
 }
