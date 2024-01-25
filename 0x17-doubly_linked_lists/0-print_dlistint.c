bcf73f4931aca16e0311510ffb80ac5f5ad3569b
tevn23 <mabulatt23@gmail.com>
2024-01-25 16:14:23 +0200

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,19 +2,20 @@
 /**
  * print_dlistint - prints all the elements of the list
  * @h: Pointer to the head of the list
- * 
+ *
  * Return: number of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
+
 	i = 0;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-        h = h->next;
-        i++;
+		h = h->next;
+		i++;
 	}
 
 	return (i);
